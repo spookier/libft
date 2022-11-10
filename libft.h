5a1175c0b18ce60fcbdfbd6bcf88a195c55ee96d
spookier <costinalexandru777@gmail.com>
2022-11-10 19:22:21 +0100

    ttt

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: acostin <acostin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 14:13:24 by acostin           #+#    #+#             */
-/*   Updated: 2022/11/10 16:43:21 by acostin          ###   ########.fr       */
+/*   Updated: 2022/11/10 19:21:25 by acostin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,6 @@ char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
 char 		*ft_strnstr(const char *big, const char *little, size_t len);
 char 		*ft_strdup(const char *str);
-char 		*ft_substr(char const *str, unsigned int start, size_t len);
+char 		*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif

c1377d1feebf8bb3fbe86e7708a9fe80188dcbee
spookier <costinalexandru777@gmail.com>
2022-11-14 17:49:42 +0100

    this split bruh...

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: acostin <acostin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 14:13:24 by acostin           #+#    #+#             */
-/*   Updated: 2022/11/10 23:15:39 by acostin          ###   ########.fr       */
+/*   Updated: 2022/11/13 18:48:06 by acostin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ char 		*ft_strdup(const char *str);
 char 		*ft_substr(char const *s, unsigned int start, size_t len);
 char 		*ft_strjoin(char const *s1, char const *s2);
 char 		*ft_strtrim(char const *s1, char const *set);
+char 		**ft_split(char const *str, char c);
 
 #endif

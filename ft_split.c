a08555738195b753edbffc051e906f39f213f82b
spookier <costinalexandru777@gmail.com>
2022-11-24 10:01:15 +0100

    ttt

--- ft_split.c
+++ ft_split.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acostin <acostin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 06:07:33 by acostin           #+#    #+#             */
+/*   Updated: 2022/11/24 06:07:34 by acostin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #include "libft.h"
 #include "stdio.h"
@@ -23,22 +35,6 @@ static	size_t	calc_2dsize(char const *str, char c)
 	}
 	return(size);
 }
-
-// *** ALLOC_TAB Explanation ***
-// fill each tab[j] with a substring from str starting from i,
-// ft_substr => fill until you find the next parameter,
-// the next parameter is ft_strchr =>
-// from the start of string + i, search c and once found, change the string's start position to start at c,
-// MINUS string + i
-
-// * If successfull, increment j ensuring you allocate a new array for the next iteration to repeat algorithm and store new value
-// and the while loop will skip the string if there's no c found ensuring no new array will be created
-
-// Example: string = AB/CD/EF, c = '/'
-// i = 0
-// 1. Create substring from str, starting from 0 until you find '/'
-// 
-// 
 
 
 static void	alloc_tab(char const *str, char **tab, char c)

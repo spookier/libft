2209bbc2a0f5e337e6c183e75986ef09d59977b9
spookier <acostin444@gmail.com>
2022-11-24 16:53:28 +0100

    yess

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: acostin <acostin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 06:08:00 by acostin           #+#    #+#             */
-/*   Updated: 2022/11/24 16:00:52 by acostin          ###   ########.fr       */
+/*   Updated: 2022/11/24 16:24:07 by acostin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,20 @@
 
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	printf("test");	
+	unsigned int	i;
+	char			*res;
+
+	if (!s || !f)
+		return (NULL);
+	res = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		res[i] = f(i, s[i]);
+		i++;
+	}
+	res[i] = 0;
+	return (res);
 }
-
-
-// char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
-// {
-// 	unsigned int	i;
-// 	char			*res;
-
-// 	if (!s || !f)
-// 		return (NULL);
-// 	res = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
-// 	if (!res)
-// 		return (NULL);
-// 	i = 0;
-// 	while (s[i])
-// 	{
-// 		res[i] = f(i, s[i]);
-// 		i++;
-// 	}
-// 	res[i] = 0;
-// 	return (res);
-// }
-

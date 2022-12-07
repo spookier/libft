598327053179a9ef310c707ea609c107e49af82f
spookier <costinalexandru777@gmail.com>
2022-12-07 16:16:54 +0100

    finished bonus part

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: acostin <acostin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 14:13:24 by acostin           #+#    #+#             */
-/*   Updated: 2022/11/26 00:35:54 by acostin          ###   ########.fr       */
+/*   Updated: 2022/12/07 15:59:49 by acostin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ typedef struct	s_list
 void			*content;
 struct s_list	*next;
 }	t_list;
+
 int			ft_isalpha(int c);
 int			ft_isdigit(int c);
 int			ft_isalnum(int c);
@@ -57,5 +58,14 @@ char 		**ft_split(char const *str, char c);
 char 		*ft_itoa(int n);
 char 		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
+t_list 		*ft_lstnew(void *content);
+void 		ft_lstadd_front(t_list **lst, t_list *new);
+int 		ft_lstsize(t_list *lst);
+t_list 		*ft_lstlast(t_list *lst);
+void 		ft_lstadd_back(t_list **lst, t_list *new);
+void 		ft_lstdelone(t_list *lst, void (*del)(void*));
+void 		ft_lstclear(t_list **lst, void (*del)(void*));
+void 		ft_lstiter(t_list *lst, void (*f)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif

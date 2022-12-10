f8ce0b1e93ef08be38072edc22048199adf5e026
spookier <costinalexandru777@gmail.com>
2022-12-10 15:20:38 +0100

    edit memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: acostin <acostin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 10:02:03 by acostin           #+#    #+#             */
-/*   Updated: 2022/11/10 10:30:13 by acostin          ###   ########.fr       */
+/*   Updated: 2022/12/10 15:20:25 by acostin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 int ft_memcmp(const void * pointer1, const void * pointer2, size_t size)
 {
-	size_t i;
-	char *p1;
-	char *p2;
-	
-	p1 = (char *)pointer1;
-	p2 = (char *)pointer2;
-	i = 0;
-	while(i < size)
-	{
-		if(p1[i] == p2[i])
-			i++;
-		else
-			return(p1 - p2);
-	}
-	return(0);
+    size_t i;
+    unsigned char *p1;
+    unsigned char *p2;
+    
+    p1 = (unsigned char *)pointer1;
+    p2 = (unsigned char *)pointer2;
+    i = 0;
+    while(i < size)
+    {
+        if(((unsigned char *)p1)[i] == ((unsigned char *)p2)[i])
+            i++;
+        else
+            return(((unsigned char *)p1)[i] - ((unsigned char *)p2)[i]);
+    }
+    return(0);
 }
